fe6766e88c8af106da9bfa9c720aee4074dbcc47
joaovitorpagani99 <joaovitorpagani7@hotmail.com>
2024-07-18 11:43:19 -0300

    construindo estrutura e fazendo login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Menu from './components/Menu';
+import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Produtos from './pages/Produtos';
+import Galeria from './pages/Galeria';
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/galeria' element={<Galeria />} />
+          <Route path='/produtos' element={<Produtos />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
