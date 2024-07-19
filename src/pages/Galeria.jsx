0334022f311989d986053013c78d8f8df51057ba
eugenio-alves <geninhoalves15@gmail.com>
2024-07-19 15:03:30 -0300

    atualização cards e galeria

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,9 +1,11 @@
+import Cards from '../components/Cards';
 import './Galeria.css'
 
 function Galeria() {
     return (
         <div>
             <h1>Galeria</h1>
+            <Cards></Cards>
         </div >
     )
 }
