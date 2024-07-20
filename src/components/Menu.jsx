b31569d479e8b11c894519fe7df5102bd456705d
joaovitorpagani99 <joaovitorpagani7@hotmail.com>
2024-07-20 19:07:37 -0300

    tela para adicionar produtos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -15,7 +15,6 @@ function Menu() {
         });
     }
 
-
     return (
         <header>
             <Navbar bg="light" expand="lg">
@@ -24,10 +23,11 @@ function Menu() {
                     <Navbar.Toggle />
                     <Navbar.Collapse>
                         <Nav className="ms-auto">
-                            {usuario && <Link className="nav-link" to='/'>Home</Link>}
+                            <Link className="nav-link" to='/'>Home</Link>
                             {!usuario && <Link className="nav-link" to='/login'>Login</Link>}
-                            {usuario && <Link className="nav-link" to='/galeria'>Galeria</Link>}
+                            <Link className="nav-link" to='/galeria'>Galeria</Link>
                             {usuario && <Link className="nav-link" to='/catalogo'>Catalogo</Link>}
+                            {usuario && usuario.email === 'admin@admin.com.br' && <Link className="nav-link" to='/adicionarProdutos'>Adicionar Produto</Link>}
                             <Link className="nav-link" to='/Sobre'>Sobre</Link>
                             {usuario && <Button variant="outline-dark" onClick={handleLogout}>
                                 Sair
@@ -37,7 +37,7 @@ function Menu() {
                 </Container>
             </Navbar>
         </header>
-    )
+    );
 }
 
 export default Menu;
--- src/firebase/produtos.js
+++ src/firebase/produtos.js
@@ -2,13 +2,19 @@ import { addDoc, collection, doc } from "firebase/firestore";
 import { db } from "./config";
 import { storage } from "./config";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-s
+
 export const colecaoProdutos = collection(db, "produtos");
 
-export async function addProdutos(data, file) {
-    const imageUrl = await uploadImagem(file);
-    const produtoData = { ...data, imageUrl };
-    await addDoc(colecaoProdutos, produtoData);
+export async function addProdutos(data, imagemUrl) {
+    data.imageUrl = imagemUrl;
+    return await addDoc(colecaoProdutos, data);
+}
+
+export async function uploadImagem(file) {
+    const storageRef = ref(storage, `imagens/${file.name}`);
+    await uploadBytes(storageRef, file);
+    const imageUrl = await getDownloadURL(storageRef);
+    return imageUrl;
 }
 
 
@@ -21,7 +27,7 @@ export async function getProdutos() {
     return produtos;
 }
 
-export async function getProdutos(id) {
+export async function getProduto(id) {
     const docRef = doc(colecaoProdutos, id);
     const snapshot = await getDoc(docRef);
     return snapshot.data();
@@ -35,13 +41,3 @@ export async function editarProduto(id, data, file) {
     const docRef = doc(colecaoProdutos, id);
     await updateDoc(docRef, data);
 }
-
-
-
-async function uploadImagem(file) {
-    const storageRef = ref(storage, `imagens/${file.name}`);
-    await uploadBytes(storageRef, file);
-    const url = await getDownloadURL(storageRef);
-    return url;
-}
-
